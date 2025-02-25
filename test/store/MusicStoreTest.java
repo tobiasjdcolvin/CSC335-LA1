145d5969675de1a334f835c4afc1a91d82775744
Tobias Colvin <107777392+tobiasjdcolvin@users.noreply.github.com>
2025-02-25 16:29:06 -0700

    Created and populated hashmaps for albums with titles as keys and albums with artists as keys.

--- test/store/MusicStoreTest.java
+++ test/store/MusicStoreTest.java
@@ -1,6 +1,7 @@
 package test.store;
 
 import org.junit.Assert;
+import src.store.Album;
 import src.store.MusicStore;
 import org.junit.Test;
 import src.store.Song;
@@ -28,5 +29,24 @@ public class MusicStoreTest {
         ArrayList<Song> currSongList = myMusicStore.getSongsByArtist("Adele");
         Assert.assertTrue(currSongList.size() == 24);
         Assert.assertEquals("daydreamer", currSongList.get(0).getTitle());
+    }
+
+    @Test
+    public void testGetAlbumByTite() {
+        // testing with the album Begin Again by Norah Jones:
+        Album currAlbum = myMusicStore.getAlbumByTitle("Begin Again");
+        // TODO: actually test stuff instead of printing string
+        System.out.println(currAlbum.toString());
+    }
+
+    @Test
+    public void testGetAlbumsByTitle() {
+        // testing with all albums by Adele (there are two Adele albums in the albums directory)
+        ArrayList<Album> currAlbumList = myMusicStore.getAlbumsByArtist("Adele");
+        // TODO: actually test stuff instead of printing string
+        for (Album a : currAlbumList) {
+            System.out.println(a.toString());
+        }
+        Assert.assertEquals(2, currAlbumList.size());
     }
 }
