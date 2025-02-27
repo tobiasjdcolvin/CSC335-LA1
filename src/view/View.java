cfa8ce7398d3e126d7ca963fa7e3cb988e8d5491
Sean <sdmccormick808@gmail.com>
2025-02-26 20:05:36 -0700

    added playlist library methods

--- src/view/View.java
+++ src/view/View.java
@@ -30,6 +30,9 @@ public class View {
                 'add song': add a song to your library (as long as it is in the store)
                 'add album': add an album to your library (as long as it is in the store)
                 'get my songs': get a list of all song titles from your library
+                'create playlist': create a playlist
+                'add to playlist': add a song to a playlist
+                'remove from playlist': remove a song from a playlist
                 """;
 
         String userInput = "";
@@ -183,6 +186,38 @@ public class View {
                     }
                 } else {
                     System.out.println("No songs in library.");
+                }
+            } else if (userInput.equals("create playlist")) {
+                System.out.println("Enter the name of the playlist you want to create:");
+                String userResponse1 = scanner.nextLine();
+
+                model.createPlaylist(userResponse1);
+                System.out.println("Successfully created playlist");
+            } else if (userInput.equals("add to playlist")) {
+                System.out.println("Enter the name of the playlist you want to add to:");
+                String userResponse1 = scanner.nextLine();
+                System.out.println("Enter the name of the song you want to add:");
+                String userResponse2 = scanner.nextLine();
+                System.out.println("Enter the name of the artist of the song you want to add:");
+                String userResponse3 = scanner.nextLine();
+
+                if (model.addSongToPlaylist(userResponse2, userResponse3, userResponse1)) {
+                    System.out.println("Successfully added song to playlist");
+                } else {
+                    System.out.println("Could not find song/playlist");
+                }
+            } else if(userInput.equals("remove from playlist")) {
+                System.out.println("Enter the name of the playlist you want to remove from:");
+                String userResponse1 = scanner.nextLine();
+                System.out.println("Enter the name of the song you want to remove:");
+                String userResponse2 = scanner.nextLine();
+                System.out.println("Enter the name of the artist of the song you want to remove:");
+                String userResponse3 = scanner.nextLine();
+
+                if (model.removeSongFromPlaylist(userResponse2, userResponse3, userResponse1)) {
+                    System.out.println("Successfully removed song from playlist");
+                } else {
+                    System.out.println("Could not find song/playlist, no changes made");
                 }
             }else {
                 System.out.println("Command not recognised, please try again.");
