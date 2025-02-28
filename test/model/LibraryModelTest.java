de6e32b72fb89e56e7c7d4d7b3ff37238b2af6d1
Tobias Colvin <107777392+tobiasjdcolvin@users.noreply.github.com>
2025-02-27 19:52:29 -0700

    Got over 90 percent coverage for tests on LibraryModel.java

--- test/model/LibraryModelTest.java
+++ test/model/LibraryModelTest.java
@@ -33,10 +33,68 @@ public class LibraryModelTest {
     }
 
     @Test
+    public void testGetPlaylistByName() {
+        testModel.addAlbumToLibrary("19", "adele");
+        testModel.createPlaylist("taco");
+        String result = testModel.getPlaylistByName("taco");
+        Assert.assertTrue(!result.equals(""));
+        String result2 = testModel.getPlaylistByName("tortilla");
+        Assert.assertTrue(result2.equals(""));
+    }
+
+    @Test
+    public void testFavoriteASong() {
+        testModel.addAlbumToLibrary("19", "adele");
+        testModel.favoriteASong("tired", "adele");
+        ArrayList<String> result = testModel.getFavorites();
+        Assert.assertEquals(1, result.size());
+        testModel.favoriteASong("not a song", "not an artist");
+        result = testModel.getFavorites();
+        Assert.assertEquals(1, result.size());
+        testModel.favoriteASong("tired", "not an artist");
+        result = testModel.getFavorites();
+        Assert.assertEquals(1, result.size());
+        testModel.favoriteASong("tired", "adele");
+        result = testModel.getFavorites();
+        Assert.assertEquals(1, result.size());
+    }
+
+    @Test
+    public void testGetAlbumsByTitleIfExists() {
+        testModel.addAlbumToLibrary("19", "adele");
+        ArrayList<String> results = testModel.getAlbumsByTitle("19");
+        Assert.assertEquals(1, results.size());
+
+    }
+
+    @Test
+    public void testGetAlbumsByArtist() {
+        testModel.addAlbumToLibrary("19", "adele");
+        ArrayList<String> results = testModel.getAlbumsByArtist("adele");
+        Assert.assertEquals(1, results.size());
+        ArrayList<String> results2 = testModel.getAlbumsByArtist("bob");
+        Assert.assertEquals(0, results2.size());
+    }
+
+    @Test
     public void testGetAlbumsByTitle() {
         ArrayList<String> albumList = testModel.getAlbumsByTitle("19");
         Assert.assertEquals(0, albumList.size());
     }
+
+    @Test
+    public void testRemoveSongFromPlaylist() {
+        testModel.addAlbumToLibrary("19", "adele");
+        testModel.createPlaylist("bog of dreams");
+        testModel.addSongToPlaylist("tired", "adele", "bog of dreams");
+        ArrayList<String> result = testModel.getPlaylists();
+        Assert.assertEquals(1, result.size());
+        testModel.removeSongFromPlaylist("tired", "adele", "bog of dreams");
+        testModel.removeSongFromPlaylist("tired", "adele", "not a playlist");
+        testModel.removeSongFromPlaylist("tired", "poopdog", "bog of dreams");
+        testModel.removeSongFromPlaylist("blah", "adele", "bog of dreams");
+    }
+
 
     @Test
     public void testGetSongsByArtist() {
@@ -58,9 +116,76 @@ public class LibraryModelTest {
     }
 
     @Test
+    public void testGetRating() {
+        testModel.addAlbumToLibrary("19", "adele");
+        String result1 = testModel.getRating("tired", "adele");
+        Assert.assertTrue(result1.equals("No rating."));
+        testModel.rateASong("tired", "adele", "599999");
+        String result2 = testModel.getRating("tired", "adele");
+        Assert.assertTrue(result2.equals("No rating."));
+        testModel.rateASong("tired", "adele", "5");
+        String result3 = testModel.getRating("tired", "adele");
+        Assert.assertFalse(result3.equals("No rating."));
+        String result4 = testModel.getRating("fjdasl", "fjsklf");
+        Assert.assertTrue(result4.equals("Song not found."));
+        String result5 = testModel.getRating("tired", "fjsklf");
+        Assert.assertFalse(result5.equals("Song not found."));
+    }
+
+    @Test
+    public void testGetPlaylists() {
+        testModel.addAlbumToLibrary("19", "adele");
+        testModel.createPlaylist("bog of dreams");
+        testModel.addSongToPlaylist("tired", "adele", "bog of dreams");
+        ArrayList<String> result = testModel.getPlaylists();
+        Assert.assertEquals(1, result.size());
+    }
+
+    @Test
+    public void testRateASongEdgeCases() {
+        testModel.addAlbumToLibrary("19", "adele");
+        boolean result = testModel.rateASong("tired", "adele", "jlkjlkj");
+        Assert.assertEquals(false, result);
+        boolean result2 = testModel.rateASong("tired", "adele", "99");
+        Assert.assertEquals(false, result2);
+        boolean result3 = testModel.rateASong("tired", "adele", "-1");
+        Assert.assertEquals(false, result3);
+        boolean result4 = testModel.rateASong("hghgh", "adele", "5");
+        Assert.assertEquals(false, result4);
+        boolean result5 = testModel.rateASong("tired", "pooprocket", "5");
+        Assert.assertEquals(false, result5);
+        boolean result6 = testModel.rateASong("tired", "adele", "3");
+        Assert.assertEquals(true, result6);
+    }
+
+    @Test
     public void testGetSongsByArtistFromStore() {
         ArrayList<String> songList = testModel.getSongsByArtistFromStore("Adele");
         Assert.assertEquals(24, songList.size());
+    }
+
+    @Test
+    public void testGetArtists() {
+        testModel.addAlbumToLibrary("19", "adele");
+        ArrayList<String> result = testModel.getArtists();
+        Assert.assertEquals(1, result.size());
+    }
+
+    @Test
+    public void testAddAlbumToLibraryEdgeCases() {
+        boolean result = testModel.addAlbumToLibrary("the album", "the artist");
+        Assert.assertEquals(false, result);
+        boolean result2 = testModel.addAlbumToLibrary("19", "adele");
+        Assert.assertEquals(true, result2);
+        boolean result3 = testModel.addAlbumToLibrary("19", "adele");
+        Assert.assertEquals(true, result3);
+    }
+
+    @Test
+    public void testGetAlbums() {
+        testModel.addAlbumToLibrary("19", "adele");
+        ArrayList<String> result = testModel.getAlbums();
+        Assert.assertEquals(1, result.size());
     }
 
     @Test
