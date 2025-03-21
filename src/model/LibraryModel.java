528c644fa61da54642a03d89e4aaa1653d26afd8
Tobias Colvin <107777392+tobiasjdcolvin@users.noreply.github.com>
2025-03-21 16:05:54 -0700

    Started implementing partial albums functionality.

--- src/model/LibraryModel.java
+++ src/model/LibraryModel.java
@@ -367,6 +367,7 @@ public class LibraryModel {
             }
         }
         if (foundAlbum != null) {
+            this.addAlbumToLibraryOnlyCurrent(foundAlbum.getTitle().toLowerCase(), foundAlbum.getArtist().toLowerCase());
             if (!this.genreTracker.containsKey("genre:"+foundAlbum.getGenre())) {
                 this.genreTracker.put("genre:"+foundAlbum.getGenre(), new Playlist("genre:"+foundAlbum.getGenre()));
             }
@@ -819,6 +820,10 @@ public class LibraryModel {
                 this.songTitlesList.add(title);
             }
         }
+    }
+
+    private void addAlbumToLibraryOnlyCurrent(String alName, String artName) {
+        // TODO HERE
     }
 
     public String shufflePlaylist(String playlistName) {
